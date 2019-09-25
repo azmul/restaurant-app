ee5d1e594d5901add9da5e61b627739e75a97f99
azmul <azmulg@gmail.com>
2019-09-26 05:57:23 +0600

    Configuration file has been included

---
+++ config/production.js
@@ -0,0 +1,11 @@
+const config = {
+  baseUrl: 'https://api.foursquare.com/v2/venues',
+  clientId: 'MJFR41Z2L4I10FOGKBKQJSGCPBJNIAMGYAUVNNNFEDYTPVYC',
+  clientSecret: 'VZOVZCDONIABSZCW1M5SKGTPYQZ5LBTYMH0NADUX5LDGXS3Q',
+  V: '20190425',
+  googleApiKey: 'AIzaSyDANxCbXZOI5RESIHd4wKiqO9mMa26bnn0',
+  googleApiUrl: 'https://maps.googleapis.com/maps/api/geocode/json?',
+  environment: 'Production',
+};
+
+export default config;
