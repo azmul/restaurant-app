16e0dfec4fd91abb55140dc2a2605a3676591f37
azmul <azmulg@gmail.com>
2019-09-26 22:24:49 +0600

    client id and client secret have been changed

--- config/development.js
+++ config/development.js
@@ -1,7 +1,7 @@
 const config = {
   baseUrl: 'https://api.foursquare.com/v2/venues',
-  clientId: 'MJFR41Z2L4I10FOGKBKQJSGCPBJNIAMGYAUVNNNFEDYTPVYC',
-  clientSecret: 'VZOVZCDONIABSZCW1M5SKGTPYQZ5LBTYMH0NADUX5LDGXS3Q',
+  clientId: '50BVFDDOR0BK0BQS4EUKLPJEKRQOKPGOC1GL5HECHPV312YU',
+  clientSecret: 'KZAWT4TZBJ2ZHUDCXZIEN3FSQPWLBG2L5Y2BKBSDKCXSKJ5Y',
   V: '20190425',
   googleApiKey: 'AIzaSyDANxCbXZOI5RESIHd4wKiqO9mMa26bnn0',
   googleApiUrl: 'https://maps.googleapis.com/maps/api/geocode/json?',
